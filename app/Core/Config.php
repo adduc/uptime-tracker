3f59e20e1fde14b37549fc090cde4cb73611595e
John Long <a@88k.us>
2013-06-06 02:00:39 -0500

    Work on config

--- app/Core/Config.php
+++ app/Core/Config.php
@@ -3,6 +3,25 @@
 namespace Adduc\Uptime\Core;
 use Adduc\Uptime\Exception;
 
-class Config {
+class Config extends Container {
+
+    protected $defaults = array(
+        'cookie_name' => '_'
+    );
+
+    public function __construct($file = null) {
+        $this->data = $this->defaults;
+        is_null($file) || $this->loadFile($file);
+    }
+
+    public function loadFile($file) {
+        $data = @parse_ini_file($file, true);
+        if(!$data) {
+            $msg = "Config file doesn't exist or is not readable.";
+            throw new Exception\ConfigNotFound($msg);
+        }
+        $this->data = $this->defaults;
+        $this->offsetSet(null, $data);
+    }
 
 }
